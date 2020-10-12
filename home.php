8c8262f7038ee1cc9790eab6c4a851c94df10246
Vicente Bruno <vicentebrunonp@outlook.com>
2020-10-12 13:51:10 -0300

    Inicialização de Projeto

---
+++ home.php
@@ -0,0 +1,3 @@
+  <div class="section">
+      <h2 class="center-align">Bem vindo ao sistema de cadastro de funcionarios</h2>
+  </div>
---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+  <title>Desafio PHP</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+<body>
+<nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="index.php" class="brand-logo">Inicio</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="cadastrar.php">Cadastro</a></li>
+      </ul>
+
+      <ul class="right hide-on-med-and-down">
+        <li><a href="funcionarios.php">Listar Funcionarios</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="cadastrar.php">Cadastro</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="funcionarios.php">Listar Funcionarios</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+  </div>
+  <div class="container">
